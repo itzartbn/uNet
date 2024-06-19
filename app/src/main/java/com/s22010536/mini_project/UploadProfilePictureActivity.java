f91921292a059c965a759f1f71e5c6a1164452c9
itzartbn <nelithnds@gmail.com>
2024-06-19 16:21:32 +0530

    Added upload profile picture functions

---
+++ app/src/main/java/com/s22010536/mini_project/UploadProfilePictureActivity.java
@@ -0,0 +1,148 @@
+package com.s22010536.mini_project;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class UploadProfilePictureActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private ImageView  imageViewUploadPic;
+    private FirebaseAuth authProfile;
+    private StorageReference storageReference;
+    private FirebaseUser firebaseUser;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri uriImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_upload_profile_picture);
+
+
+
+        Button picChooseBtn = findViewById(R.id.pic_choose_btn);
+        Button uploadPicBtn = findViewById(R.id.upload_picture_btn);
+        progressBar = findViewById(R.id.progressBar_profile);
+        imageViewUploadPic = findViewById(R.id.imageview_profile_dp);
+
+        authProfile = FirebaseAuth.getInstance();
+        firebaseUser = authProfile.getCurrentUser();
+
+        storageReference = FirebaseStorage.getInstance().getReference("DisplayPics");
+
+        Uri uri = firebaseUser.getPhotoUrl();
+
+        //set users current dp if already uploaded
+        Picasso.get().load(uri).into(imageViewUploadPic);
+
+//choosing the image button
+        picChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        //upload image to firebase
+        uploadPicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                UploadPic();
+
+            }
+        });
+
+    }
+
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    private void UploadPic() {
+        if (uriImage != null) {
+            //save the image with the uid of currently logged user
+            StorageReference fileReference = storageReference.child(authProfile.getCurrentUser().getUid() + "." + getFileExtension(uriImage));
+
+            //upload image to storage
+            fileReference.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Uri downloadUri = uri;
+                            firebaseUser = authProfile.getCurrentUser();
+
+
+                            //setting pp after user upload a one
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(downloadUri).build();
+                            firebaseUser.updateProfile(profileUpdates);
+                        }
+                    });
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(UploadProfilePictureActivity.this, "Profile Picture Set Successfuly", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(UploadProfilePictureActivity.this, UserProfileActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadProfilePictureActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
+    //obtaiing the file extention of the image
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uriImage = data.getData();
+            imageViewUploadPic.setImageURI(uriImage);
+        }
+    }
+
+
+}
--- app/src/main/java/com/s22010536/mini_project/UserProfileActivity.java
+++ app/src/main/java/com/s22010536/mini_project/UserProfileActivity.java
@@ -1,21 +1,16 @@
 package com.s22010536.mini_project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.widget.Toolbar;
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,39 +19,45 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    private TextView textViewWelcome, textViewFullname, textViewEmail, textViewProgram, textViewSid, textViewLocation;
+    private TextView textViewFullname, textViewEmail, textViewProgram, textViewSid, textViewLocation;
     private ProgressBar progressBar;
     private String fullName, email, sid, program, location;
-    private ImageView imageView;
+    private ShapeableImageView profileDp;
     private FirebaseAuth authProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_profile);
 
-        //textViewWelcome = findViewById(R.id.showWelcome);
         textViewFullname = findViewById(R.id.profile_fullName);
         textViewEmail = findViewById(R.id.profile_email);
         textViewSid = findViewById(R.id.profile_sid);
         textViewProgram = findViewById(R.id.profile_program);
         textViewLocation = findViewById(R.id.profile_location);
         progressBar = findViewById(R.id.progressBar_profile);
-        imageView = findViewById(R.id.profileDp);
-        //Toolbar toolBar = findViewById(R.id.toolbar);
-
-
-        //setSupportActionBar(toolBar);
-        //getSupportActionBar().setTitle("Profile");
-
-
+        profileDp = findViewById(R.id.profileDp);
+        Button profileLogoutbtn = findViewById(R.id.profile_logout_btn);
 
         authProfile = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+        profileLogoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authProfile.signOut();
+                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         if (firebaseUser == null){
             Toast.makeText(UserProfileActivity.this, "Oops!, Something went wrong", Toast.LENGTH_LONG).show();
@@ -65,34 +66,54 @@ public class UserProfileActivity extends AppCompatActivity {
             showUserProfile(firebaseUser);
         }
 
-
+        profileDp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent uploadpicIntent = new Intent(UserProfileActivity.this, UploadProfilePictureActivity.class);
+                startActivity(uploadpicIntent);
+            }
+        });
     }
-
-
 
     private void showUserProfile(FirebaseUser firebaseUser) {
         String userId = firebaseUser.getUid();
-
-        //Extracting user reference from database for "registered users"
         DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
         referenceProfile.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ReadWriteUserDetails readUserDetails = snapshot.getValue(ReadWriteUserDetails.class);
-                if (readUserDetails != null){
+                if (readUserDetails != null) {
                     fullName = firebaseUser.getDisplayName();
                     email = firebaseUser.getEmail();
                     sid = readUserDetails.sid;
                     program = readUserDetails.program;
                     location = readUserDetails.location;
 
-                    //textViewWelcome.setText("Welcome " + fullName + "!");
                     textViewFullname.setText(fullName);
                     textViewEmail.setText(email);
                     textViewSid.setText(sid);
                     textViewProgram.setText(program);
                     textViewLocation.setText(location);
 
+//set profile picture
+                    Uri uri = firebaseUser.getPhotoUrl();
+                    Picasso.get()
+                            .load(uri)
+                            .placeholder(R.drawable.user_icon)
+                            .into(profileDp, new Callback() {
+                                @Override
+                                public void onSuccess() {
+                                    // Handle success
+                                }
+
+                                @Override
+                                public void onError(Exception e) {
+                                    // Handle error
+                                    Toast.makeText(UserProfileActivity.this, "Failed to load profile picture", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(UserProfileActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
                 }
                 progressBar.setVisibility(View.GONE);
             }
@@ -101,34 +122,7 @@ public class UserProfileActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(UserProfileActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
                 progressBar.setVisibility(View.GONE);
-
             }
         });
-
-
     }
-
-    /*//create actionbar menu
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //inflate menu items
-        getMenuInflater().inflate(R.menu.common_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    //when any menu item is selected
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.menu_refresh){
-            //refreash
-            startActivity(getIntent());
-            finish();
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }*/
 }
