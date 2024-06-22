23e12dd5cbd61bebae03dfe4dfff244103ad5fa5
itzartbn <nelithnds@gmail.com>
2024-06-23 00:32:10 +0530

    implemented camera activity for profile pic upload (with some bugs)

--- app/src/main/java/com/s22010536/mini_project/UploadProfilePictureActivity.java
+++ app/src/main/java/com/s22010536/mini_project/UploadProfilePictureActivity.java
@@ -1,19 +1,30 @@
 package com.s22010536.mini_project;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,17 +35,21 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
-import android.widget.Toast;
 
 public class UploadProfilePictureActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
+    private static final int CAMERA_PERMISSION_CODE = 100;
 
     private ProgressBar progressBar;
     private ImageView imageViewUploadPic;
     private FirebaseAuth authProfile;
     private StorageReference storageReference;
     private FirebaseUser firebaseUser;
-    private static final int PICK_IMAGE_REQUEST = 1;
     private Uri uriImage;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,7 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_upload_profile_picture);
 
         Button picChooseBtn = findViewById(R.id.pic_choose_btn);
+        Button cameraBtn = findViewById(R.id.camera_btn);
         Button uploadPicBtn = findViewById(R.id.upload_picture_btn);
         progressBar = findViewById(R.id.progressBar_profile);
         imageViewUploadPic = findViewById(R.id.imageview_profile_dp);
@@ -53,11 +69,10 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReference("DisplayPics");
 
         Uri uri = firebaseUser.getPhotoUrl();
+        if (uri != null) {
+            Picasso.get().load(uri).into(imageViewUploadPic);
+        }
 
-        // Set user's current profile picture if already uploaded
-        Picasso.get().load(uri).into(imageViewUploadPic);
-
-        // Choosing the image button
         picChooseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,7 +80,13 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
             }
         });
 
-        // Upload image to Firebase
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
         uploadPicBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,14 +101,28 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
+
+    }
+
+    private void openCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, CAMERA_PERMISSION_CODE);
+        } else {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "New Picture");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+            uriImage = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriImage);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+
+        }
     }
 
     private void uploadPic() {
         if (uriImage != null) {
-            // Save the image with the uid of currently logged user
             StorageReference fileReference = storageReference.child(authProfile.getCurrentUser().getUid() + "." + getFileExtension(uriImage));
-
-            // Upload image to storage
             fileReference.putFile(uriImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -96,8 +131,6 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
                         public void onSuccess(Uri uri) {
                             Uri downloadUri = uri;
                             firebaseUser = authProfile.getCurrentUser();
-
-                            // Setting profile picture after user uploads one
                             UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                                     .setPhotoUri(downloadUri).build();
                             firebaseUser.updateProfile(profileUpdates);
@@ -105,7 +138,6 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
                     });
                     progressBar.setVisibility(View.GONE);
                     Toast.makeText(UploadProfilePictureActivity.this, "Profile Picture Set Successfully", Toast.LENGTH_SHORT).show();
-
                     Intent intent = new Intent(UploadProfilePictureActivity.this, UserProfileActivity.class);
                     startActivity(intent);
                     finish();
@@ -113,13 +145,13 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(UploadProfilePictureActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
     }
 
-    // Obtaining the file extension of the image
     private String getFileExtension(Uri uri) {
         ContentResolver cR = getContentResolver();
         MimeTypeMap mime = MimeTypeMap.getSingleton();
@@ -133,6 +165,20 @@ public class UploadProfilePictureActivity extends AppCompatActivity {
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             uriImage = data.getData();
             imageViewUploadPic.setImageURI(uriImage);
+        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageViewUploadPic.setImageURI(uriImage);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
